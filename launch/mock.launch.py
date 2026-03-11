2d09e93cbbed3c7d8bc626d635aad7ba9c41b834
creeper5820 <cqq1960180796@163.com>
2026-03-11 21:37:04 +0800

    improve: add standard tf transform

---
+++ launch/mock.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    bag_path = "/workspaces/data/bag/battlefield_0/"
+    play_bag = ExecuteProcess(
+        cmd=["ros2", "bag", "play", bag_path],
+        output="screen",
+    )
+
+    pkg_share = get_package_share_directory("point_lio")
+    point_lio = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([pkg_share, "launch", "point_lio.launch.py"])
+        )
+    )
+    return LaunchDescription([point_lio, play_bag])
